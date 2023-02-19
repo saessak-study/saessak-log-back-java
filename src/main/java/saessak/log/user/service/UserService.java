1ea795f4f065e96934a8219430040a81e4569033
gunhee93 <123151812+gunhee93@users.noreply.github.com>
2023-02-19 23:29:26 +0900

    join-login
    - 회원가입, 로그인, 중복확인 service, controller 설계 수정
    - 회원가입, 중복확인 Test 성공

--- src/main/java/saessak/log/user/service/UserService.java
+++ src/main/java/saessak/log/user/service/UserService.java
@@ -5,6 +5,9 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import saessak.log.user.User;
 import saessak.log.user.dto.UserDto;
+import saessak.log.user.dto.UserDuplicateDto;
+import saessak.log.user.dto.UserJoinDto;
+import saessak.log.user.dto.UserLoginDto;
 import saessak.log.user.repository.UserRepository;
 
 import java.util.List;
@@ -19,20 +22,28 @@ public class UserService {
 
     // 회원가입
     @Transactional
-    public Long join(UserDto userDto){
-        User user = userDto.toEntity();
+    public Long join(UserJoinDto userJoinDto){
+        User user = userJoinDto.toEntity();
         userRepository.save(user);
-        return userDto.getId();
-
+        return user.getId();
     }
 
     // profileId 중복검사
-    public void duplicateUser(User user) {
-        List<User> findUser = (List<User>) userRepository.findByProfileId(user.getProfileId());
-        if(!findUser.isEmpty()){
-            throw new IllegalStateException("중복된 아이디입니다.");
-        }
+    public void duplicateUser(UserDuplicateDto userDuplicateDto) {
+        userRepository.findOptionalByProfileId(userDuplicateDto.getProfileId())
+                .ifPresent(u -> {
+                    throw new IllegalStateException("중복된 아이디입니다.");
+                });
+    }
 
+    // 로그인
+    public Boolean login(UserLoginDto userLoginDto){
+        User findUser = userRepository.findOptionalByProfileId(userLoginDto.getProfileId())
+                .orElseThrow();
+        if(findUser.getPassword().equals(userLoginDto.getPassword())){
+            return true;
+        }
+        return false;
     }
 
     // 회원정보 수정
