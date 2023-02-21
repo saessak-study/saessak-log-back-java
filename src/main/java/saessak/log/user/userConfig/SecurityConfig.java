484e4e9538d1ceaff5a07f47757869cfb959eac2
gunhee93 <123151812+gunhee93@users.noreply.github.com>
2023-02-22 01:17:13 +0900

    User-Security
    비밀번호 암호화를 위한
    - security implementation 추가
    - security config class 추가
    - spring security 정책이 바뀌어서 권한에 대한 자료와 이해가 부족해 우선 작업 종료 하였습니다. 내일 추가 작업 예정입니다.

--- src/main/java/saessak/log/user/userConfig/SecurityConfig.java
+++ src/main/java/saessak/log/user/userConfig/SecurityConfig.java
@@ -1,18 +1,45 @@
 package saessak.log.user.userConfig;
 
+import antlr.BaseAST;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
+
+    @Bean
+    private PasswordEncoder encoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public InMemoryUserDetailsManager userDetailsService() {
+        UserDetails admin = User.withUsername("admin")
+                .password(encoder().encode("adminPass"))
+                .roles("ADMIN")
+                .build();
+        UserDetails user = User.withUsername("user")
+                .password(encoder().encode("userPass"))
+                .roles("USER")
+                .build();
+        return new InMemoryUserDetailsManager(admin, user);
+    }
+
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.authorizeRequests().antMatchers("/login").permitAll()
                 .antMatchers("/users/**", "/settings/**").hasAuthority("Admin")
-                .hasAnyAuthority("Admin", "Editor", "Salesperson")
-                .hasAnyAuthority("Admin", "Editor", "Salesperson", "Shipper")
+//                .hasAnyAuthority("Admin", "Editor", "Salesperson")
+//                .hasAnyAuthority("Admin", "Editor", "Salesperson", "Shipper")
                 .anyRequest().authenticated()
                 .and().formLogin()
                 .loginPage("/login")
