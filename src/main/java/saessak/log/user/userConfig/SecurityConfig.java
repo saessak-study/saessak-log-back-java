72c2005a72a6f538dc553c91880d129e7f60ba33
youhobin <ghdb132@naver.com>
2023-02-25 14:34:51 +0900

    feat : cors-error
    컨트롤러에 CrossOrigin 어노테이션 추가

--- src/main/java/saessak/log/user/userConfig/SecurityConfig.java
+++ src/main/java/saessak/log/user/userConfig/SecurityConfig.java
@@ -45,7 +45,7 @@ public class SecurityConfig {
                 .and()
                     .authorizeRequests()
                         .antMatchers("/user").permitAll() // user 권한 허용 // 이거 다시
-                        .antMatchers("/user/join", "/user/login").permitAll() // join, login 허용
+                        .antMatchers("/user/join", "/user/login", "/user/duplicate", "/user/findId", "/user/resetPassword").permitAll() // join, login 허용
                 .antMatchers(HttpMethod.POST, "/comments/**", "/posts/**").authenticated() // 아니면 /*까지 해보기.
 //                .antMatchers(HttpMethod.GET, "/coments", "/posts").authenticated()
                 .and()
