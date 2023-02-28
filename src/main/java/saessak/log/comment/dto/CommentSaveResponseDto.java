5d78839603030995da86c9eae0f0a3cacb7da1ad
youhobin <ghdb132@naver.com>
2023-02-28 21:12:18 +0900

    feat : comment
    commet 저장시 commentId 값 보내주기, 댓글페이징 api 에 commentId 값 보내주기

---
+++ src/main/java/saessak/log/comment/dto/CommentSaveResponseDto.java
@@ -0,0 +1,11 @@
+package saessak.log.comment.dto;
+
+
+public class CommentSaveResponseDto {
+
+    private Long commentId;
+
+    public CommentSaveResponseDto(Long commentId) {
+        this.commentId = commentId;
+    }
+}
