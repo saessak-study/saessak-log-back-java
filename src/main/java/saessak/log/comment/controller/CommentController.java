2ee6ebed0a466440205a2a486a434081996f35c1
youhobin <ghdb132@naver.com>
2023-03-01 13:13:18 +0900

    Feature/comment (#38)
    * feat : save-post
    
    이미지 저장 시 응답 postId로 보내주기
    
    * feat : main-page
    
    메인페이지에 유저 profileId값 추가
    
    * feat : comment
    
    commet 저장시 commentId 값 보내주기, 댓글페이징 api 에 commentId 값 보내주기

--- src/main/java/saessak/log/comment/controller/CommentController.java
+++ src/main/java/saessak/log/comment/controller/CommentController.java
@@ -18,6 +18,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import saessak.log.comment.Comment;
 import saessak.log.comment.dto.CommentSaveDto;
+import saessak.log.comment.dto.CommentSaveResponseDto;
 import saessak.log.comment.dto.CommentViewDto;
 import saessak.log.comment.service.CommentService;
 
@@ -33,9 +34,10 @@ public class CommentController {
     @ApiOperation(value = "댓글 작성")
     @ApiResponse(code = 200, message = "댓글을 작성하셨습니다.", response = String.class)
     @PostMapping("/comment")
-    public ResponseEntity saveComment(@RequestBody CommentSaveDto commentSaveDto) {
-        commentService.saveComment(commentSaveDto);
-        return ResponseEntity.ok("댓글을 작성하셨습니다.");
+    public ResponseEntity<CommentSaveResponseDto> saveComment(@RequestBody CommentSaveDto commentSaveDto) {
+        Long savedComment = commentService.saveComment(commentSaveDto);
+        CommentSaveResponseDto commentSaveResponseDto = new CommentSaveResponseDto(savedComment);
+        return ResponseEntity.ok().body(commentSaveResponseDto);
     }
 
     @ApiOperation(value = "댓글 페이징")
