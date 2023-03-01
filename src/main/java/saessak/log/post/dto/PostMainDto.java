afa0eb52e61ab63277e4d437d432252beac156ad
youhobin <ghdb132@naver.com>
2023-03-01 13:12:14 +0900

    Feature/save post (#37)
    * feat : save-post
    
    이미지 저장 시 응답 postId로 보내주기
    
    * feat : main-page
    
    메인페이지에 유저 profileId값 추가

--- src/main/java/saessak/log/post/dto/PostMainDto.java
+++ src/main/java/saessak/log/post/dto/PostMainDto.java
@@ -7,13 +7,15 @@ import lombok.Setter;
 public class PostMainDto {
 
     private Long postId;
+    private String profileId;
     private String imageFile;
     private long commentCount;
     private long reactionCount;
 
-    public PostMainDto(long postId, String imageFile, long commentCount, long reactionCount) {
+    public PostMainDto(long postId, String profileId, String imageFile, long commentCount, long reactionCount) {
         this.postId = postId;
-        this.imageFile = imageFile;
+        this.profileId = profileId;
+        this.imageFile = "https://saessaklogfile.s3.ap-northeast-2.amazonaws.com/image/" + imageFile;
         this.commentCount = commentCount;
         this.reactionCount = reactionCount;
     }
